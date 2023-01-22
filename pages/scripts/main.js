287366b3fe8ac8a86363035baf8fbb7bbb83f6a2
Pyogenics <py0g3n1cs@gmail.com>
2023-01-22 12:52:34 +0000

    Initial propyl uploading

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -1,1 +1,44 @@
-import * as THREE from 'three';
+/*
+ *   Web based map editor for tanki online
+ *   Copyright (C) 2023  Pyogenics <https://github.com/Pyogenics>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+window.onload = main;
+
+function main()
+{
+	console.log("Starting.....");
+
+	/* Propyl upload */
+	const submitBtn = document.getElementById("upload");
+	const fileArea = document.getElementById("propyl-folder");
+	const reader = new FileReader();
+
+	submitBtn.fileArea = fileArea;
+	submitBtn.addEventListener("click", loadPropyl);
+
+	console.log("Done!");
+}
+
+/* Propyl events */
+function loadPropyl(event)
+{
+	console.log("Loading propyl....");
+	for (const file of event.target.fileArea.files)
+	{
+		console.log(file.webkitRelativePath);
+	}
+}
